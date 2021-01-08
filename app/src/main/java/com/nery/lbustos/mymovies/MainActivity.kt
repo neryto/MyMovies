2931519eaac5f4c4faf014fe2267ff6da530b699
Nery Bustos <nery.bustos@lennken.com>
2021-01-08 16:34:44 -0600

    view binding implementation

--- app/src/main/java/com/nery/lbustos/mymovies/MainActivity.kt
+++ app/src/main/java/com/nery/lbustos/mymovies/MainActivity.kt
@@ -3,7 +3,6 @@ package com.nery.lbustos.mymovies
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.nery.lbustos.mymovies.databinding.ActivityMainBinding
 import okhttp3.OkHttpClient
@@ -17,10 +16,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
 
 
-    var movies : ArrayList<MovieItem> = arrayListOf()
-    private var adapter : MoviesAdapter?  = null
+    var movies: ArrayList<MovieItem> = arrayListOf()
+    private var adapter: MoviesAdapter? = null
     private lateinit var binding: ActivityMainBinding
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,44 +30,48 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-   private fun initView(){
+    private fun initView() {
 
-       binding.recyclerViewMovies
-       binding.recyclerViewMovies.layoutManager = StaggeredGridLayoutManager(3,
-           StaggeredGridLayoutManager.VERTICAL)
+        binding.recyclerViewMovies
+        binding.recyclerViewMovies.layoutManager = StaggeredGridLayoutManager(
+            3,
+            StaggeredGridLayoutManager.VERTICAL
+        )
         adapter = MoviesAdapter(movies) {
             BottomSheetDetailFragment(it)
                 .apply {
-                show(supportFragmentManager,BottomSheetDetailFragment.TAG)
-            }
+                    show(supportFragmentManager, BottomSheetDetailFragment.TAG)
+                }
         }
-       binding.recyclerViewMovies?.adapter = adapter
+        binding.recyclerViewMovies?.adapter = adapter
     }
 
 
-    fun getMovies(){
-        getRetrofit().create(MoviesWebservices :: class.java)
-            .getMovies("117d1bd56ea2f98e9662d5cfbd5408e2").enqueue(object  : Callback<MoviesResponse>{
+    fun getMovies() {
+        getRetrofit().create(MoviesWebservices::class.java)
+            .getMovies("117d1bd56ea2f98e9662d5cfbd5408e2")
+            .enqueue(object : Callback<MoviesResponse> {
                 override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
-                    Log.d("TAG1",t.localizedMessage)
+                    Log.d("TAG1", t.localizedMessage)
                 }
 
                 override fun onResponse(
                     call: Call<MoviesResponse>,
                     response: Response<MoviesResponse>
                 ) {
-                   movies.addAll( response.body()!!.results)
-                   adapter?.notifyDataSetChanged()
+                    movies.addAll(response.body()!!.results)
+                    adapter?.notifyDataSetChanged()
                 }
 
             })
     }
 
 
-    fun getRetrofit() : Retrofit {
-       val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-       val httpClientBuilder = OkHttpClient.Builder().addInterceptor(loggingInterceptor!!)
-       return Retrofit.Builder()
+    fun getRetrofit(): Retrofit {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val httpClientBuilder = OkHttpClient.Builder().addInterceptor(loggingInterceptor!!)
+        return Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/")
             .client(httpClientBuilder?.build())
             .addConverterFactory(GsonConverterFactory.create())
